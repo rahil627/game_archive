70b57db3a07acbfe50940a02f8158250ef309259
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-08-17 01:11:49 -0700

    gen fake data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -45,10 +45,9 @@
 defmodule GameArchive.GameMaker do
 
   alias GameArchive.Game
-  alias Faker
+  alias Faker # strange library
 
-  # def gen_game do # TODO: to elixir.txt NOTE: can't define a function outside a module!?
-  # ... but you can define a module in the script!
+
   def make_game do
     %Game { # this must be a tuple.. {key: value} NOTE: syntax requires the space
       title: "test",
@@ -68,46 +67,42 @@ defmodule GameArchive.GameMaker do
   # generates this sql
   # INSERT INTO "games" ("title","tags","alt_titles","makers","studios","countries","languages","release_year","genres","short_description","tech_tags","inserted_at","updated_at") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) RETURNING "id" ["test", ["string 1", "string 2"], ["string 1", "string 2"], ["string 1", "string 2"], ["string 1", "string 2"], ["string 1", "string 2"], ["string 1", "string 2"], 1999, ["string 1", "string 2"], "this is a description", ["string 1", "string 2"], ~U[2024-08-12 09:13:45Z], ~U[2024-08-12 09:13:45Z]]
 
-  def make_games do # TODO: NOTE: function doesn't need (), but the call does
-    # loop gen_game
-    # TODO: may have to learn list comprehensions
-    gen_game()
-  end
+  # def make_games do # TODO: NOTE: function doesn't need (), but the call does
+  #   # TODO: how to loop..?
+  #   Enum.each(1..100, gen_game())
+  # end
 
-  defp gen_game do # LOVE the defp syntax!
-    # TODO: generate using faker lib
+  def gen_game do # LOVE the defp syntax!
      %Game { # this must be a tuple.. {key: value} NOTE: syntax requires the space
-      title: Faker.Pokemon.En.name(),
-      alt_titles: ["string 1", "string 2"], # i think "arrays" are just lists in elixir..
-      makers: ["string 1", "string 2"],
-      studios: ["string 1", "string 2"],
-      countries: ["string 1", "string 2"],
-      languages: ["string 1", "string 2"],
+      title: Faker.Lorem.word(),
+      alt_titles: [Faker.Lorem.word(), Faker.Lorem.word()], # i think "arrays" are just lists in elixir..
+      makers: [Faker.Person.name(), Faker.Person.name()],
+      studios: [Faker.Lorem.word(), Faker.Lorem.word()],
+      countries: [Faker.Lorem.word(), Faker.Lorem.word()],
+      languages: [Faker.Lorem.word(), Faker.Lorem.word()],
       release_year: 1999,
-      genres: ["string 1", "string 2"],
-      tags: ["string 1", "string 2"],
-      short_description: "this is a description",
-      tech_tags: ["string 1", "string 2"],
+      genres: [Faker.Lorem.word(), Faker.Lorem.word()],
+      tags: [Faker.Lorem.word(), Faker.Lorem.word()],
+      short_description: Faker.Lorem.sentence(5..10),
+      tech_tags: [Faker.Lorem.word(), Faker.Lorem.word()],
     }
  end
 
 end
-
-
-
-
 
 # main script starts here
 alias GameArchive.Repo
 alias GameArchive.GameMaker
 # alias GameArchive.Games
 
-Repo.insert!(GameMaker.make_game())
+for n <- 1..10 do
+  Repo.insert!(GameMaker.gen_game())
+end
 
 
 
 
-
+# testing stuff
 # game2 = Games.create_game() # TODO: what are attrs?
 # TODO: error:
 
